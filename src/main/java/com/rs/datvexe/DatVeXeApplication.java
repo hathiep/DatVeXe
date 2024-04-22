1a9dce4fc4efc311e6b1345676412a7dc541992b
hathiep <havanthiep2308@gmail.com>
2024-04-22 22:55:27 +0700

    First commit - Upload project

---
+++ src/main/java/com/rs/datvexe/DatVeXeApplication.java
@@ -0,0 +1,13 @@
+package com.rs.datvexe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatVeXeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatVeXeApplication.class, args);
+	}
+
+}
