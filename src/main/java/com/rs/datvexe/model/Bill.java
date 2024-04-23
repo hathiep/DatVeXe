a006b6de1eeb93ffac9d9b340a9e202e7daf3329
hathiep <havanthiep2308@gmail.com>
2024-04-23 17:03:29 +0700

    Update

--- src/main/java/com/rs/datvexe/model/Bill.java
+++ src/main/java/com/rs/datvexe/model/Bill.java
@@ -28,16 +28,24 @@ public class Bill {
     @Column(name = "quantity")
     private int quantity;
 
+    @Column(name = "don")
+    private String don;
+
+    @Column(name = "tra")
+    private String tra;
+
     public Bill() {
     }
 
-    public Bill(int id, int idTrip, Date date, Time time, int idClient, int quantity) {
+    public Bill(int id, int idTrip, Date date, Time time, int idClient, int quantity, String don, String tra) {
         this.id = id;
         this.idTrip = idTrip;
         this.date = date;
         this.time = time;
         this.idClient = idClient;
         this.quantity = quantity;
+        this.don = don;
+        this.tra = tra;
     }
 
     public int getId() {
@@ -86,5 +94,21 @@ public class Bill {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    public String getDon() {
+        return don;
+    }
+
+    public void setDon(String don) {
+        this.don = don;
+    }
+
+    public String getTra() {
+        return tra;
+    }
+
+    public void setTra(String tra) {
+        this.tra = tra;
     }
 }
--- src/main/java/com/rs/datvexe/repository/LocationRepository.java
+++ src/main/java/com/rs/datvexe/repository/LocationRepository.java
@@ -1,13 +1,13 @@
 package com.rs.datvexe.repository;
 
-import com.rs.datvexe.model.Route;
+import com.rs.datvexe.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface RouteRepository extends JpaRepository<Route, Integer> {
+public interface LocationRepository extends JpaRepository<Location, Integer> {
 
-    List<Route> getByIdTripAndType(Integer idTrip, String type);
+    List<Location> getByIdRouteAndType(Integer idRoute, String type);
 }
